a81f893ab94c66e692c59abbc6ede52d63e6db06
unknown <matthew.p.luttrell@gmail.com>
2016-04-28 19:58:46 -0400

    adding initial puppet file for docker host to mount persistent data directories

---
+++ docker-host.pp
@@ -0,0 +1,30 @@
+file { '/puntonthird/logs':
+    ensure => 'directory',
+    owner  => 'root',
+    mode   => 'go+w',
+}
+  
+file { '/puntonthird/mongodata':
+    ensure => 'directory',
+    owner  => 'root',
+    mode   => 'go+w',
+}
+
+mount { 'puntonthird log mount':
+	name 	=> '/puntonthird/logs',
+	ensure 	=> 'mounted',
+	device 	=> '/dev/sdc1',
+	fstype	=> 'ext4',
+	options => 'defaults',
+	require => File['/puntonthird/logs']
+}
+
+mount { 'mongo data mount':
+	name 	=> '/puntonthird/mongodata',
+	ensure 	=> 'mounted',
+	device 	=> '/dev/sdd1',
+	fstype	=> 'ext4',
+	options => 'defaults',
+	require => File['/puntonthird/logs']
+}
+
